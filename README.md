4dd22637d45ad9f6895a8bb148e822ce971d348b
devabhimanyu <dev.chauhan.singh.abhimanyu@gmail.com>
2021-10-09 12:37:57 +0530

    hello world app

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # go-basics
+
 Basics of golang
+
+- go run main.go : compiles and immediately run Go program
+
+- go build : compile go src files. Compiles the packeages and dependencies
+  - go build -o name : will produce and exec file with the name entered
---
+++ hello-world/main.go
@@ -0,0 +1,23 @@
+// a package clause starts every source file
+// main is a special name declaring an executable rather than
+package main
+
+// import declaration declares packages used in this file
+import "fmt"
+
+// package scoped variables and constants
+const secondsInHour = 3600
+
+// a function declaration. main is a special function name
+// it is the entry point for the executable program
+// Go uses brace brackets to delimit code blocks
+func main(){
+	// Println() function prints out a line to stdout
+	fmt.Println("Hello Go")
+	
+	// Local Scoped Variables and Constants Declarations, calling functions etc
+	distance := 60.8;
+	
+	fmt.Printf("the distance in miles is %f \n", distance*0.62137)
+
+}
